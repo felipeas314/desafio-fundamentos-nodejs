5673a67ed574cf52f8c05baf62d1464e621dbbfb
Felipe Alexandre <felipeas314@gmail.com>
2020-04-18 18:58:40 -0300

    primeiro commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,11 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +19,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((sum, transaction) => {
+      if (transaction.type === 'income') {
+        return (sum += transaction.value);
+      }
+      return sum;
+    }, 0);
+
+    const outcome = this.transactions.reduce((sum, transaction) => {
+      if (transaction.type === 'outcome') {
+        return (sum += transaction.value);
+      }
+      return sum;
+    }, 0);
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,30 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({ title, value, type }: RequestDTO): Transaction {
     // TODO
+
+    if (type === 'outcome' && !this.checkIfCanAdd(value)) {
+      throw Error('No add transactions');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+
+  private checkIfCanAdd(value: number): boolean {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (total < value) {
+      return false;
+    }
+
+    return true;
   }
 }
 
